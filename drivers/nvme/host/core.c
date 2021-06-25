831a833362ed96aef3ca61dd414f3c6ad017f481
Hannes Reinecke <hare@suse.de>
2021-06-25 08:08:18 +0200

    nvme: use separate subsystem ida
    The subsystem and the controller are really two distinct entities, and
    can have vastly different lifetimes. So there is only limited sense in
    using the same IDA for enumarating both, and we should rather employ
    different IDA instances for each.
    This simplifies the code to allocate and deallocate instance numbers
    for both controller and subsystems.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- drivers/nvme/host/core.c
+++ drivers/nvme/host/core.c
@@ -128,6 +128,7 @@ static LIST_HEAD(nvme_subsystems);
 DEFINE_MUTEX(nvme_subsystems_lock);
 
 static DEFINE_IDA(nvme_instance_ida);
+static DEFINE_IDA(nvme_subsystem_ida);
 static dev_t nvme_ctrl_base_chr_devt;
 static int nvme_class_uevent(const struct device *dev, struct kobj_uevent_env *env);
 static const struct class nvme_class = {
@@ -2909,8 +2910,7 @@ static void nvme_release_subsystem(struct device *dev)
 	struct nvme_subsystem *subsys =
 		container_of(dev, struct nvme_subsystem, dev);
 
-	if (subsys->instance >= 0)
-		ida_free(&nvme_instance_ida, subsys->instance);
+	ida_free(&nvme_subsystem_ida, subsys->instance);
 	kfree(subsys);
 }
 
@@ -3006,7 +3006,12 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 	if (!subsys)
 		return -ENOMEM;
 
-	subsys->instance = -1;
+	ret = ida_alloc(&nvme_subsystem_ida, GFP_KERNEL);
+	if (ret < 0) {
+		kfree(subsys);
+		return ret;
+	}
+	subsys->instance = ret;
 	mutex_init(&subsys->lock);
 	kref_init(&subsys->ref);
 	INIT_LIST_HEAD(&subsys->ctrls);
@@ -3028,6 +3033,7 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 		dev_err(ctrl->device,
 			"Subsystem %s is not a discovery controller",
 			subsys->subnqn);
+		ida_free(&nvme_subsystem_ida, subsys->instance);
 		kfree(subsys);
 		return -EINVAL;
 	}
@@ -3037,7 +3043,7 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 	subsys->dev.class = &nvme_subsys_class;
 	subsys->dev.release = nvme_release_subsystem;
 	subsys->dev.groups = nvme_subsys_attrs_groups;
-	dev_set_name(&subsys->dev, "nvme-subsys%d", ctrl->instance);
+	dev_set_name(&subsys->dev, "nvme-subsys%d", subsys->instance);
 	device_initialize(&subsys->dev);
 
 	mutex_lock(&nvme_subsystems_lock);
@@ -3070,8 +3076,6 @@ static int nvme_init_subsystem(struct nvme_ctrl *ctrl, struct nvme_id_ctrl *id)
 		goto out_put_subsystem;
 	}
 
-	if (!found)
-		subsys->instance = ctrl->instance;
 	ctrl->subsys = subsys;
 	list_add_tail(&ctrl->subsys_entry, &subsys->ctrls);
 	mutex_unlock(&nvme_subsystems_lock);
@@ -4794,8 +4798,8 @@ static void nvme_free_ctrl(struct device *dev)
 		container_of(dev, struct nvme_ctrl, ctrl_device);
 	struct nvme_subsystem *subsys = ctrl->subsys;
 
-	if (!subsys || ctrl->instance != subsys->instance)
-		ida_free(&nvme_instance_ida, ctrl->instance);
+	ida_free(&nvme_instance_ida, ctrl->instance);
+
 	nvme_free_cels(ctrl);
 	nvme_mpath_uninit(ctrl);
 	cleanup_srcu_struct(&ctrl->srcu);
@@ -5191,6 +5195,7 @@ static void __exit nvme_core_exit(void)
 	destroy_workqueue(nvme_wq);
 	ida_destroy(&nvme_ns_chr_minor_ida);
 	ida_destroy(&nvme_instance_ida);
+	ida_destroy(&nvme_subsystem_ida);
 }
 
 MODULE_LICENSE("GPL");
