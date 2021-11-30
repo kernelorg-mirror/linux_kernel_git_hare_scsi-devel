0b2b3c5a178387661495198cc1245543fbc03223
Hannes Reinecke <hare@suse.de>
2021-11-30 08:11:12 +0100

    scsi: don't check for non-empty inquiry data

--- drivers/scsi/scsi_sysfs.c
+++ drivers/scsi/scsi_sysfs.c
@@ -500,8 +500,7 @@ static void scsi_device_dev_release_usercontext(struct work_struct *work)
 		kfree_rcu(vpd_pg80, rcu);
 	if (vpd_pg89)
 		kfree_rcu(vpd_pg89, rcu);
-	if (!scsi_device_is_host_dev(sdev))
-		kfree(sdev->inquiry);
+	kfree(sdev->inquiry);
 	kfree(sdev);
 
 	if (parent)
