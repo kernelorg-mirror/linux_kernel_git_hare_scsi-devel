2f9635844a163239304dbb22b32d38c702246c6e
Hannes Reinecke <hare@suse.de>
2023-03-21 09:02:21 +0100

    nvme-keyring: define a 'psk' keytype
    Define a 'psk' keytype to hold the NVMe TLS PSKs.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- include/linux/nvme-keyring.h
+++ include/linux/nvme-keyring.h
@@ -6,6 +6,14 @@
 #ifndef _NVME_KEYRING_H
 #define _NVME_KEYRING_H
 
+#include <linux/key.h>
+
+struct key *nvme_tls_psk_lookup(key_ref_t keyring,
+				const char *hostnqn, const char *subnqn,
+				int hmac, bool generated);
+
+key_serial_t nvme_keyring_id(void);
+
 int nvme_keyring_init(void);
 void nvme_keyring_exit(void);
 
