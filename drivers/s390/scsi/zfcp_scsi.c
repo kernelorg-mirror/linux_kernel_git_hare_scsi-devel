3c541d296cc7f482b913518e9c00931c3cbfa224
Hannes Reinecke <hare@suse.de>
2023-10-02 17:08:34 +0200

    zfcp: do not wait for rports to become unblocked after host reset
    zfcp_scsi_eh_host_reset_handler() would call fc_block_rport() to
    wait for all rports to become unblocked after host reset.
    But after host reset it might happen that the port is gone, hence
    fc_block_rport() might fail due to a missing port.
    But that's a perfectly legal operation; on FC remote ports might
    come and go.
    In the same vein FC HBAs are able to deal with ports being temporarily
    blocked, so really there is not point in waiting for all ports
    to become unblocked during host reset.
    Hence remove the call to fc_block_rport() in host reset.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Cc: Steffen Maier <maier@linux.ibm.com
    Cc: Benjamin Block <bblock@linux.ibm.com>

--- drivers/s390/scsi/zfcp_scsi.c
+++ drivers/s390/scsi/zfcp_scsi.c
@@ -383,10 +383,6 @@ static int zfcp_scsi_eh_host_reset_handler(struct scsi_cmnd *scpnt)
 	}
 	zfcp_erp_adapter_reopen(adapter, 0, "schrh_1");
 	zfcp_erp_wait(adapter);
-	fc_ret = fc_block_scsi_eh(scpnt);
-	if (fc_ret)
-		ret = fc_ret;
-
 	zfcp_dbf_scsi_eh("schrh_r", adapter, ~0, ret);
 	return ret;
 }
